6aa6348b6eff362b9b5132cdc752aeb7059228b4
Ted Alexander <tedalexander93@gmail.com>
2021-10-21 16:37:40 -0400

    added files with placeholder info

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Display from "./components/Display";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return <Display />;
+}
+
+export default App;
