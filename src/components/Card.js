86080f924ead8b70f97173d06920fb493e5ae7ea
Ted Alexander <tedalexander93@gmail.com>
2021-10-21 20:09:50 -0400

    added data

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,19 +5,14 @@ export default function Card(props) {
     width: '18rem',
   };
 
-  // Helper function that generates a random width for our placeholder images
-  const randomWidth = () => {
-    const number = Math.random() * (300 - 200) + 200;
-    return number.toString().split('.')[0];
-  };
 
   return (
-    <div>
-      <div className="card" style={cardStyle}>
+    <div className="row">
+      <div className="card col-sm-4"  style={cardStyle}>
         <img
           className="card-img-top"
-          src={`http://placecorgi.com/${randomWidth()}`}
-          alt="Card cap"
+          src= {props.image}
+          alt="project image"
         />
         <div className="card-body">
           <h5 className="card-title">{props.name}</h5>
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,29 +1,51 @@
 import React from 'react';
 import Card from './Card';
 
-const canines = [
+const projects = [
   {
-    name: 'Spot',
-    description: 'The best boy',
+    name: 'S.H.I.E.L.D. Database',
+    description: 'Displays information on searched comic book characters using data from Superhero API (Requires CORS Anywhere access to use.)',
+    image: 'shield-logo.png',
     id: 1,
   },
   {
-    name: 'Zero',
-    description: 'A kind soul',
+    name: "CalenDon't",
+    description: '"The Antisocial Social Planner"',
+    image: 'calendont.JPG',
     id: 2,
   },
   {
-    name: 'Walter',
-    description: 'Friend for life',
+    name: 'Workday Scheduler',
+    description: 'A listmaker that marks passed hours in real time.',
+    image: 'workday.PNG',
     id: 3,
   },
+  {
+    name: 'Flag Quiz',
+    description: 'A listmaker that marks passed hours in real time.',
+    image: 'globe_flag.png',
+    id: 4,
+  },
+  {
+    name: 'Password Generator',
+    description: 'A listmaker that marks passed hours in real time.',
+    image: '03.png',
+    id: 5,
+  },
+  {
+    name: 'Weather Dashboard',
+    description: 'A listmaker that marks passed hours in real time.',
+    image: '',
+    id: 6,
+  },
+
 ];
 
 export default function Display() {
   return (
     <div>
-      {canines.map((dog) => (
-        <Card name={dog.name} description={dog.description} key={dog.id} />
+      {projects.map((item) => (
+        <Card name={item.name} description={item.description} image={item.image} key={item.id} />
       ))}
     </div>
   );
