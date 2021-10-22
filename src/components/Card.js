a7c678a451d40c2ef9357a4256514df4bc6927e2
Ted Alexander <tedalexander93@gmail.com>
2021-10-21 22:04:43 -0400

    filled in fields. buttons work now.

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,18 +7,21 @@ export default function Card(props) {
 
 
   return (
-    <div className="row">
+    <div>
       <div className="card col-sm-4"  style={cardStyle}>
         <img
           className="card-img-top"
-          src= {props.image}
+          src= {props.image} 
           alt="project image"
         />
         <div className="card-body">
           <h5 className="card-title">{props.name}</h5>
           <p className="card-text">{props.description}</p>
-          <a href="#" className="btn btn-primary">
-            Adopt {props.name}
+          <a href={props.deploy} className="btn btn-primary">
+             Link to the Site
+          </a>
+          <a href={props.github} className="btn btn-primary">
+            Github Link
           </a>
         </div>
       </div>
--- src/components/Display.js
+++ src/components/Display.js
@@ -6,36 +6,48 @@ const projects = [
     name: 'S.H.I.E.L.D. Database',
     description: 'Displays information on searched comic book characters using data from Superhero API (Requires CORS Anywhere access to use.)',
     image: 'shield-logo.png',
+    deploy: 'https://endlessashley.github.io/Shield-ID/',
+    github: 'https://github.com/endlessashley/Shield-ID',
     id: 1,
   },
   {
     name: "CalenDon't",
     description: '"The Antisocial Social Planner"',
     image: 'calendont.JPG',
+    deploy: 'https://pacific-badlands-48977.herokuapp.com/',
+    github: 'https://github.com/endlessashley/Shield-ID',
     id: 2,
   },
   {
     name: 'Workday Scheduler',
     description: 'A listmaker that marks passed hours in real time.',
     image: 'workday.PNG',
+    deploy: 'https://endlessashley.github.io/Shield-ID/',
+    github: 'https://github.com/rjr2/CalenDont',
     id: 3,
   },
   {
     name: 'Flag Quiz',
     description: 'A listmaker that marks passed hours in real time.',
     image: 'globe_flag.png',
+    deploy: 'https://endlessashley.github.io/Shield-ID/',
+    github: 'https://github.com/endlessashley/Shield-ID',
     id: 4,
   },
   {
     name: 'Password Generator',
     description: 'A listmaker that marks passed hours in real time.',
     image: '03.png',
+    deploy: 'https://endlessashley.github.io/Shield-ID/',
+    github: 'https://github.com/endlessashley/Shield-ID',
     id: 5,
   },
   {
     name: 'Weather Dashboard',
     description: 'A listmaker that marks passed hours in real time.',
     image: '',
+    deploy: 'https://endlessashley.github.io/Shield-ID/',
+    github: 'https://github.com/endlessashley/Shield-ID',
     id: 6,
   },
 
@@ -45,7 +57,7 @@ export default function Display() {
   return (
     <div>
       {projects.map((item) => (
-        <Card name={item.name} description={item.description} image={item.image} key={item.id} />
+        <Card name={item.name} description={item.description} image={item.image} link={item.deploy} github={item.github} key={item.id} />
       ))}
     </div>
   );
